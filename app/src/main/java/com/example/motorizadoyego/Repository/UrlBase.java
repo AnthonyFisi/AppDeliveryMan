df42bc7273422b18c33bd141b9ebf177574b01db
jhon <jhon.curi@unmsm.edu.pe>
2020-12-14 21:33:41 -0500

    Add modified details

--- app/src/main/java/com/example/motorizadoyego/Repository/UrlBase.java
+++ app/src/main/java/com/example/motorizadoyego/Repository/UrlBase.java
@@ -2,6 +2,6 @@ package com.example.motorizadoyego.Repository;
 
 public class UrlBase {
 
-    public  static final String URL_BASE= "https://backend-tiend-go.herokuapp.com/";
+    public  static final String URL_BASE= "";
 
 }
